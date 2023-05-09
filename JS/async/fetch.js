63476d54161b35643d6feb0a0f11ead9d9b1f1e4
Edwar-Sanz <ssanzjhon5@gmail.com>
2023-05-08 21:54:05 -0500

    se agregan ejemplos fetch

--- JS/async/fetch.js
+++ JS/async/fetch.js
@@ -61,4 +61,47 @@ async function asyncAwaitSintaxis (){
 
 }
 
+//------------------------mas ejemplos con peticiones http------------------------------
+
+// get
+fetch("https://jsonplaceholder.typicode.com/posts")
+.then((response) => response.json())
+.then((json) => console.log(json));
+
+// POST y PATCH.
+fetch("https://jsonplaceholder.typicode.com/posts", {
+  method: "POST",
+  body: JSON.stringify({
+    title: "Hello World",
+    body: "My POST request",
+    userId: 900,
+  }),
+  headers: {
+    "Content-type": "application/json; charset=UTF-8",
+  },
+})
+.then((response) => response.json())
+.then((json) => console.log(json));
+
+//PUT 
+fetch("https://jsonplaceholder.typicode.com/posts/1", {
+  method: "PUT",
+  body: JSON.stringify({
+    id: 1,
+    title: "My PUT request",
+    body: "Updating the entire object",
+    userId: 1,
+  }),
+  headers: {
+    "Content-type": "application/json; charset=UTF-8",
+  },
+})
+.then((response) => response.json())
+.then((json) => console.log(json));
+
+//DELETE
+fetch("https://jsonplaceholder.typicode.com/posts/3", {
+  method: "DELETE",
+});
+
 
